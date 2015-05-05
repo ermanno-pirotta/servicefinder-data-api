f79c153859d799458c91027c40fcd3e27789280d
piro84 <ermanno.pirotta@gmail.com>
2015-05-05 11:23:48 +0200

    first commit of the application skeleton generated with spring boot

---
+++ src/main/java/servicefinder/data/api/ServicefinderDataApiApplication.java
@@ -0,0 +1,12 @@
+package servicefinder.data.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicefinderDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicefinderDataApiApplication.class, args);
+    }
+}
---
+++ src/test/java/servicefinder/data/api/ServicefinderDataApiApplicationTests.java
@@ -0,0 +1,18 @@
+package servicefinder.data.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ServicefinderDataApiApplication.class)
+@WebAppConfiguration
+public class ServicefinderDataApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
