98c3603ccc37976d606f1c567d9ad5cb6016d243
piro84 <ermanno.pirotta@gmail.com>
2015-05-14 11:25:57 +0200

    - handling the different property source for test environments using the @TestPropertySource annotation

--- src/test/java/servicefinder/data/api/ControllerTest.java
+++ src/test/java/servicefinder/data/api/ControllerTest.java
@@ -10,21 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import servicefinder.data.ServicefinderDataApiTest;
+import servicefinder.data.ServicefinderDataApiApplication;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ServicefinderDataApiTest.class)
+@SpringApplicationConfiguration(classes = ServicefinderDataApiApplication.class)
 @WebAppConfiguration
-@ActiveProfiles("test")
+@TestPropertySource("/application-test.properties")
 public abstract class ControllerTest {
 	protected MediaType contentType = new MediaType(
 			MediaType.APPLICATION_JSON.getType(),
